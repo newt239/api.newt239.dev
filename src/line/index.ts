050bd7a417a41a2bb51440f25c01f99d995f319b
newt239 <ysti2019.win2020.st@gmail.com>
2022-12-11 17:43:34 +0900

    add: reply message using openai

--- src/line/index.ts
+++ src/line/index.ts
@@ -4,6 +4,7 @@ import {
   WebhookEvent,
 } from "@line/bot-sdk";
 import { Hono } from "hono";
+import { Configuration, OpenAIApi } from "openai";
 
 import { LineWebhookRequest } from "../types";
 const line = new Hono();
@@ -12,10 +13,11 @@ line.post("/webhook", async (c) => {
   const data: LineWebhookRequest = await c.req.json();
   const events: WebhookEvent[] = data.events;
   const accessToken: string = c.env.CHANNEL_ACCESS_TOKEN;
+  const openaiSecret: string = c.env.OPENAI_SECRET;
   await Promise.all(
     events.map(async (event: WebhookEvent) => {
       try {
-        await textEventHandler(event, accessToken);
+        await textEventHandler(event, accessToken, openaiSecret);
       } catch (err: unknown) {
         if (err instanceof Error) {
           console.error(err);
@@ -26,27 +28,39 @@ line.post("/webhook", async (c) => {
       }
     })
   );
-  return c.json({ message: "" });
+  return c.json({ message: "ok" });
 });
 
 const textEventHandler = async (
   event: WebhookEvent,
-  accessToken: string
+  accessToken: string,
+  openaiSecret: string
 ): Promise<MessageAPIResponseBase | undefined> => {
   if (event.type !== "message" || event.message.type !== "text") {
     return;
   }
 
-  const { replyToken } = event;
-  const { text } = event.message;
+  const configuration = new Configuration({
+    apiKey: openaiSecret,
+  });
+  const openai = new OpenAIApi(configuration);
+  const result = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: event.message.text,
+    temperature: 0.3,
+    max_tokens: 500,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  });
 
   const response: TextMessage = {
     type: "text",
-    text,
+    text: result.data.choices[0].text || "何らかのエラーが発生しました。",
   };
   await fetch("https://api.line.me/v2/bot/message/reply", {
     body: JSON.stringify({
-      replyToken: replyToken,
+      replyToken: event.replyToken,
       messages: [response],
     }),
     method: "POST",

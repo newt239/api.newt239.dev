5e57a857168595bdfe63ee00138b3f4b4f544d7b
newt239 <ysti2019.win2020.st@gmail.com>
2023-08-15 21:52:43 +0900

    remove: openai route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,10 @@
 import { Hono } from "hono";
 
 import lab from "~/routes/lab";
-import openaiRoute from "~/routes/openai";
 import spotify from "~/routes/spotify";
 
 const app = new Hono();
 app.get("/", (c) => c.text("🔥"));
-app.route("/openai", openaiRoute);
 app.route("/spotify", spotify);
 app.route("/lab", lab);
 

27626766d0ca4be4a0dd3c42fd817297ea431f96
newt <ysti2019.win2020.st@gmail.com>
2025-10-23 19:28:59 +0900

    feat: add spotify client

--- src/routes/spotify/index.ts
+++ src/routes/spotify/index.ts
@@ -2,53 +2,41 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 
 import type { Bindings } from "~/types/bindings";
-import type { SpotifyMyTopTrackProps } from "~/types/spotify";
 
-import { getSpotifyAccessToken } from "~/libs/spotify";
+import { createSpotifyClient } from "~/clients/spotify";
 
 const spotifyRoute = new Hono<{ Bindings: Bindings }>()
   .use("*", cors())
   .get("/my-top-tracks", async (c) => {
-    const SPOTIFY_CLIENT_ID: string = c.env.SPOTIFY_CLIENT_ID;
-    const SPOTIFY_CLIENT_SECRET: string = c.env.SPOTIFY_CLIENT_SECRET;
-    const REFRESH_TOKEN: string = c.env.REFRESH_TOKEN;
-
-    const accessToken = await getSpotifyAccessToken(
-      SPOTIFY_CLIENT_ID,
-      SPOTIFY_CLIENT_SECRET,
-      REFRESH_TOKEN
+    const client = createSpotifyClient(
+      c.env.SPOTIFY_CLIENT_ID,
+      c.env.SPOTIFY_CLIENT_SECRET
     );
-
-    const requestOptions = {
-      method: "GET",
-      headers: { Authorization: `Bearer ${accessToken}` },
-    };
-    const res = await fetch(
-      "https://api.spotify.com/v1/me/top/tracks",
-      requestOptions
-    );
-    const data = (await res.json()) as { items: SpotifyMyTopTrackProps[] };
-
-    const returnData = [];
-    if (data) {
-      for (const track of data.items) {
-        const artists = [];
-        for (const artist of track.artists) {
-          artists.push(artist.name);
-        }
-        const eachData = {
-          name: track.name,
-          artists: artists,
-          thumbnail: track.album.images[0].url,
-          preview: track.preview_url ? track.preview_url : null,
-          duration: track.duration_ms,
-          popularity: track.popularity,
-          link: track.external_urls.spotify,
-        };
-        returnData.push(eachData);
-      }
+    const { items } = await client.currentUser.topItems("tracks", "short_term");
+    if (!items) {
+      return c.json([]);
     }
+    const returnData = items.map((track) => {
+      return {
+        name: track.name,
+        artists: track.artists.map((artist) => artist.name),
+        thumbnail: track.album.images[0].url,
+        preview: track.preview_url ? track.preview_url : null,
+        duration: track.duration_ms,
+        popularity: track.popularity,
+        link: track.external_urls.spotify,
+      };
+    });
     return c.json(returnData);
+  })
+  .get("/search", async (c) => {
+    const query = c.req.query("query");
+    const client = createSpotifyClient(
+      c.env.SPOTIFY_CLIENT_ID,
+      c.env.SPOTIFY_CLIENT_SECRET
+    );
+    const result = await client.search(query || "test", ["artist"]);
+    return c.json(result);
   });
 
 export default spotifyRoute;
